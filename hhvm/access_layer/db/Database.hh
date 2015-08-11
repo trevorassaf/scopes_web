e8b09efcf9534bf18e69224364e8f3ab434cbaca
Trevor Assaf <trevor.assaf@gmail.com>
2015-08-10 23:07:19 -0400

    database start

--- hhvm/access_layer/db/Database.hh
+++ hhvm/access_layer/db/Database.hh
@@ -3,20 +3,15 @@
 class Database {
 
   private DbConfig $config;
-  private bool $isOpen;
-  private bool $isTransaction;
-
+  private ?AsyncMysqlConnection $connection;
 
   public function __construct(
     DbConfig $config
   ) {
     $this->config = $config;
-    $this->isOpen = false;
-    $this->isTransaction = false;
+    $this->connection = null;
   }
 
-  public function beginTxn(): void {}
-  public function endTxn(): void {}
   public function open(): void {}
   public function close(): void {}
 
