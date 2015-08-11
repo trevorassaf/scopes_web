951338212f73323d43cbdfed3fd1b4c0a539c2f6
Trevor Assaf <trevor.assaf@gmail.com>
2015-08-11 16:55:12 -0400

    changed primary key of join tables to key set

--- sql/create_tables/user_order_edges.sql
+++ sql/create_tables/user_order_edges.sql
@@ -2,11 +2,10 @@
  * Users/Orders join table
  */
 CREATE TABLE UserOrderEdges (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY(id),
   userId INT UNSIGNED NOT NULL,
   FOREIGN KEY(userId) REFERENCES Users(id),
   orderId INT UNSIGNED NOT NULL,
-  FOREIGN KEY(orderId) REFERENCES Orders(id)
+  FOREIGN KEY(orderId) REFERENCES Orders(id),
+  PRIMARY KEY(userId, orderId)
 );
 
--- sql/create_tables/user_user_privilege_edges.sql
+++ sql/create_tables/user_user_privilege_edges.sql
@@ -2,11 +2,10 @@
  * User privileges/users join table
  */
 CREATE TABLE UserUserPrivilegeEdges (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY(id),
   userId INT UNSIGNED NOT NULL,
   FOREIGN KEY(userId) REFERENCES Users(id),
   userPrivilegeId INT UNSIGNED NOT NULL,
-  FOREIGN KEY(userPrivilegeId) REFERENCES UserPrivileges(id)
+  FOREIGN KEY(userPrivilegeId) REFERENCES UserPrivileges(id),
+  PRIMARY KEY(userId, userPrivilegeId)
 );
 
