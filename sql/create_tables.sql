d63ad3ca1e883921ef439f351aa271d1cb1c9854
Trevor Assaf <trevor.assaf@gmail.com>
2015-08-12 14:43:44 -0400

    order configuration

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -2,6 +2,7 @@
 source create_tables/users.sql
 source create_tables/user_privileges.sql
 source create_tables/orders.sql
+source create_tables/order_configuration.sql
 
 source create_tables/user_user_privilege_edges.sql
 source create_tables/user_order_edges.sql
---
+++ sql/create_tables/order_configuration.sql
@@ -0,0 +1,8 @@
+/**
+ * Order configuration
+ */
+CREATE TABLE OrderConfiguration (
+  minutesPerIntervalCount INT UNSIGNED NOT NULL,
+  pricePerIntervalCount INT UNSIGNED NOT NULL,
+  scopesCount INT UNSIGNED NOT NULL
+);
