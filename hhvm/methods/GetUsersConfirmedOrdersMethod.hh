a52810852ae153c9d7efdcf7d31c82fcf36883fc
Trevor Assaf <trevor.assaf@gmail.com>
2015-08-24 12:47:52 -0400

    build get users confirmed orders method on generic fetch query

--- hhvm/methods/GetUsersConfirmedOrdersMethod.hh
+++ hhvm/methods/GetUsersConfirmedOrdersMethod.hh
@@ -1,18 +1,19 @@
 <?hh // strict
 
 class GetUsersConfirmedOrdersMethod {
-
+  
   public function __construct(
     private FetchUsersConfirmedOrdersQuery $fetchUsersConfirmedOrdersQuery
   ) {}
 
-  public function getConfirmedOrders(
+  public function get(
     UnsignedInt $user_id
   ): ImmVector<ConfirmedOrder> {
-    $query_handle = $this
+    // TODO do error checking
+    $query_wait_handle = $this
       ->fetchUsersConfirmedOrdersQuery
-      ->fetch($user_id);
-    return $query_handle
+      ->fetch($user_id);     
+    return $query_wait_handle->getWaitHandle()
       ->getWaitHandle()
       ->join();
   }
