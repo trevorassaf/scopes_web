8e5abac19b64f79317d084471776c213d0020563
Trevor Assaf <trevor.assaf@gmail.com>
2015-08-26 12:23:25 -0400

    added delete rsvd order method

---
+++ hhvm/methods/DeleteReservedOrderMethod.hh
@@ -0,0 +1,16 @@
+<?hh // strict
+
+class DeleteReservedOrderMethod {
+
+  public function __construct(
+    private DeleteByIdQuery<ReservedOrder> $deleteOrderQuery
+  ) {}
+
+  public function deleteReservedOrder(
+    UnsignedInt $rsvd_order_id
+  ): void {
+    try {
+      $this->deleteOrderQuery->delete($rsvd_order_id); 
+    } catch (QueryException $ex) {}
+  }
+}
