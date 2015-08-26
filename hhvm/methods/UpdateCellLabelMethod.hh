d4368cff6e3ed28142b09b5e253285e8af154d44
Trevor Assaf <trevor.assaf@gmail.com>
2015-08-26 12:45:42 -0400

    cell label update method

---
+++ hhvm/methods/UpdateCellLabelMethod.hh
@@ -0,0 +1,20 @@
+<?hh // strict
+
+class UpdateCellLabelMethod {
+
+  public function __construct(
+    private UpdateByIdQuery<CellLabel>  $updateQuery
+  ) {}
+
+  public function update(
+    ImmMap<string, mixed> $update_params,
+    UnsignedInt $id
+  ): void {
+    try {
+      $this->updateQuery
+        ->update($update_params, $id)
+        ->genWaitHandle()
+        ->join();
+    } catch (QueryException $ex) {} 
+  }
+}
