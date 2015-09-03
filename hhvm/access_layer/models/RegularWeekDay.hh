11d563f30fe6bab9525abeed42cd07070a0a828e
Trevor Assaf <trevor.assaf@gmail.com>
2015-09-03 13:02:53 -0400

    incoroporated day of the week type into regular week day

--- hhvm/access_layer/models/RegularWeekDay.hh
+++ hhvm/access_layer/models/RegularWeekDay.hh
@@ -2,11 +2,21 @@
 
 class RegularWeekDay implements Model {
 
+  public static function fromType(
+    DayOfTheWeekType $type
+  ): RegularWeekDay {
+    return new RegularWeekDay(new UnsignedInt((int)$type));
+  }
+
   public function __construct(
     private UnsignedInt $id
   ) {}
 
   public function getId(): UnsignedInt {
     return $this->id;
+  }
+
+  public function getType(): DayOfTheWeekType {
+    return DayOfTheWeekType::assert($this->id->getNumber());
   }
 }
