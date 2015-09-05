af560b9783b8bd958ca8e8bf7d2c432eae8152f3
Trevor Assaf <trevor.assaf@gmail.com>
2015-09-04 22:24:18 -0400

    terms

--- hhvm/access_layer/queries/where_clause_2/terms/comparison/ComparisonTerm.hh
+++ hhvm/access_layer/queries/where_clause_2/terms/comparison/ComparisonTerm.hh
@@ -3,12 +3,12 @@
 abstract class ComparisonTerm implements MySqlTerm {
 
   public function __construct(
-    private mixed $left,
-    private mixed $right
+    private string $left,
+    private string $right
   ) {} 
 
   public function serialize(): string {
-  
+    return "(" . $this->left . " " . $this->getComparisonToken() . " " . $this->right . ")";
   }
 
   abstract protected function getComparisonToken(): string; 
