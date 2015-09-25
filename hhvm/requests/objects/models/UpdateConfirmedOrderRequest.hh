69e87e73e71887e010edfa6d90cf2fa9511232fd
Trevor Assaf <astrev@umich.edu>
2015-09-25 12:46:22 -0700

    update confirmed order model

---
+++ hhvm/requests/objects/models/UpdateConfirmedOrderRequest.hh
@@ -0,0 +1,94 @@
+<?hh // strict
+
+class UpdateConfirmedOrderRequest {
+
+  const string REQUEST_OBJECT_NAME = "UpdateConfirmedOrderRequest";
+
+  const string CONFIRMED_ORDER_ID_KEY = 'cid';
+  const string TITLE_KEY = 'title';
+  const string DESCRIPTION_KEY = 'desc';
+  const string SHORT_CODE_KEY = 'code';
+  const string RECORDING_DURATION_KEY = 'duration';
+  const string CELL_LABEL_REQUESTS_KEY = 'labels';
+
+  public function __construct(
+    private RequestField<UnsignedInt> $confirmedOrderId,
+    private ?RequestField<string> $title,
+    private ?RequestField<string> $description,
+    private ?RequestField<string> $shortCode,
+    private ?RequestField<UnsignedInt> $recordingDuration
+  ) {}
+
+  public function getConfirmedOrderId(): RequestField<UnsignedInt> {
+    return $this->confirmedOrderId;
+  }
+
+  public function getTitle(): ?RequestField<string> {
+    return $this->title;
+  }
+
+  public function getDescription(): ?RequestField<string> {
+    return $this->description;
+  }
+
+  public function getShortCode(): ?RequestField<string> {
+    return $this->shortCode;
+  }
+
+  public function getRecordingDuration(): ?RequestField<UnsignedInt> {
+    return $this->recordingDuration;
+  }
+}
+
+class UpdateConfirmOrderRequestBuilder {
+  
+  private ?RequestField<UnsignedInt> $confirmedOrderId;
+  private ?RequestField<string> $title;
+  private ?RequestField<string> $description;
+  private ?RequestField<string> $shortCode;
+  private ?RequestField<UnsignedInt> $recordingDuration;
+
+  public function setConfirmedOrderId(?RequestField<UnsignedInt> $confirmed_order_id): this {
+    $this->confirmedOrderId = $confirmed_order_id;
+    return $this; 
+  }
+
+  public function setTitle(?RequestField<string> $title): this {
+    $this->title = $title;
+    return $this;
+  }
+
+  public function setDescription(?RequestField<string> $description): this {
+    $this->description = $description;
+    return $this;
+  }
+  
+  public function setShortCode(?RequestField<string> $short_code): this {
+    $this->shortCode = $short_code;
+    return $this;
+  }
+
+  public function setRecordingDuration(?RequestField<UnsignedInt> $recording_duration): this {
+    $this->recordingDuration = $recording_duration;
+    return $this;
+  }  
+
+  public function build(): UpdateConfirmedOrderRequest {
+    // Confirmed order request must be provided!
+    if ($this->confirmedOrderId === null) {
+      throw new UnsetRequestFieldException(
+        UpdateConfirmedOrderRequest::REQUEST_OBJECT_NAME,
+        UpdateConfirmedOrderRequest::CONFIRMED_ORDER_ID_KEY
+      );
+    }
+
+    // Extrude request object and ensure fields are not null
+    return new UpdateConfirmedOrderRequest(
+      $this->confirmedOrderId,
+      $this->title,
+      $this->description,
+      $this->shortCode,
+      $this->recordingDuration
+    );
+  }
+}
