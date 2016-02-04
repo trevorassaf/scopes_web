77622da962ccc573d877c8b2ba3ea694495b5720
Trevor Assaf <astrev@umich.edu>
2016-02-04 14:56:12 -0800

    added calendar to page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 window.onload = function() {
 
+  var disallowed_week_days = new Set([0, 6]);
+  var calendar = new Calendar(
+      'exp-date-calendar',
+      disallowed_week_days,
+      {},
+      3,
+      0 
+  );
+  calendar.init();
+
   /**
    * Configure UI elements
    */
